7bf8fdfd4a496d0a7d3429bc755a5ba06fb2d0a6
NicolasHarnisch <nicolasgomeshar@gmail.com>
2025-10-02 13:14:27 -0300

    code class 4

---
+++ Conta Bancaria - Aula 04/src/Pessoa.java
@@ -0,0 +1,13 @@
+class Pessoa {
+	
+	String nome;
+	String cpf;
+	int idade;
+	
+	Pessoa(String nome, String cpf, int idade){
+		this.nome = nome;
+		this.cpf = cpf;
+		this.idade = idade;
+	}
+	
+}
